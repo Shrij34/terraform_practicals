7df61a1e6b8135693348c377d08c1923bf49d07f
Shrij34 <54050818+Shrij34@users.noreply.github.com>
2020-09-04 22:59:33 +0530

    Update base.tf

--- base/base.tf
+++ base/base.tf
@@ -13,6 +13,19 @@ user_data= <<-EOF
               echo "hey i am $(hostname -f)" > /var/www/html/index.html
               service httpd start
               chkconfig httpd on
+
+sudo yum -y update
+
+echo "Install Java JDK 8"
+yum remove -y java
+yum install -y java-1.8.0-openjdk
+
+echo "Install Jenkins"
+wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+yum install -y jenkins
+sudo chkconfig jenkins on
+sudo service jenkins start
 EOF
 tags = {
    Name = "jmsth21"
